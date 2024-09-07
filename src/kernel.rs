a56f806a741dc43bb3c897f3238bb9f432a1016d
Shakari Wade <saturn.kari.9@gmail.com>
2024-09-07 16:44:01 -0700

    Coding bootloader to make kernel self-booting, a source for this has been added to the readme. Readme also updated with spec changes.

---
+++ src/kernel.rs
@@ -0,0 +1,36 @@
+// import other modules
+#![no_std]
+#![no_main]
+/*mod boot {
+    use core::arch::asm;
+    use core::arch::global_asm;
+    global_asm!(
+        ".section .text._start"
+    );
+
+    
+}*/
+
+
+use core::panic::PanicInfo;
+use core::arch::asm;
+
+static HELLO: &[u8] = b"Hello World!";
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
