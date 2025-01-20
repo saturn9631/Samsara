292b582eeab01d04a3542235174dabb6b84eb34a
Shakari Wade <saturn.kari.9@gmail.com>
2025-01-20 11:10:53 -0800

    Added a module to deal with the first stage.

--- compiler/src/parser.rs
+++ compiler/src/parser.rs
@@ -2,9 +2,12 @@ use std::fmt::Formatter;
 use std::fmt::Display;
 use std::fmt::Result;
 
+mod stage_one;
+use stage_one::Scentences;
+
 pub struct Parser {
     characters: Vec<char>,
-    ast_stage_1: Vec<Tokens>,
+    ast_stage_1: Scentences,
     //ast_stage_2: Vec<>,
 } impl Parser {
     pub fn new(lines: Vec<String>) -> Self {
@@ -16,57 +19,11 @@ pub struct Parser {
         }
         Self {
             characters: pieces,
-            ast_stage_1: Vec::new(),
+            ast_stage_1: Scentences::new(),
             //ast_stage_2: Vec::new(),
         }
     }
     fn generate_stage_1(&mut self) {
-        let mut first_space = true; //To stop multiple pushes of blank words. 
-        let mut nested = 0;
-        for character in self.characters {
-            if character == '(' {
-                self.ast_stage_1.push(Tokens::Block(1, Vec::new()));
-                first_space = true;
-                nested += 1
-            } else if character == ')' {
-                first_space = true;
-                nested -= 1
-            } else if character == '[' {
-                self.ast_stage_1.push(Tokens::Block(1, Vec::new()));
-                nested += 1
-            } else if character == ']' {
-                first_space = true;
-                nested -= 1
-            } else if character == '{' {
-                self.ast_stage_1.push(Tokens::Block(1, Vec::new()));
-                nested += 1
-            } else if character == '}' {
-                first_space = true;
-                nested -= 1
-            } else if character == ' ' || character == '\n' || character == '\t' {
-                if first_space {
-                    self.ast_stage_1.push(Tokens::Word(String::new()));
-                    first_space = false;
-                }
-            } else {
-                let mut possible_container = self.ast_stage_1.last_mut();
-                match possible_target {
-                    Some(target) => {
-                        target.push(character);
-                    }
-                    None => {
-                        println!("An error occured accessing the latatst token in the list.");
-                    }
-                }
-            }
-        }
-        if nested != 0 {
-            if nested > 0 {
-                println!("You need to close some blocks");
-            } else {
-                println!("You have too many block closers");
-            }
-        }
     }
 
     fn generate_stage_2(&mut self) {
@@ -80,15 +37,7 @@ pub struct Parser {
         }
     }
 
-    pub fn print_tokens(&self) {
-        let mut index: u8 = 0;
-        for token in &self.tokens {
-            println!("Token {index}: {token}");
-            index += 1;
-        }
-    }
-    
-    pub fn print_stage_1(&self) {
+    pub fn print_words(&self) {
     }
 
     pub fn print_stage_2(&self) {
@@ -96,22 +45,3 @@ pub struct Parser {
 } /*impl Display for Parser {
 }*/
 
-enum Tokens {
-    Word(String),
-    Block(Int, Vec<Tokens>),
-} impl Tokens {
-
-    fn push(&mut self, character: char) {
-        match &self {
-            Tokens::Word(identifier) => {
-                identifier.push(character);
-            }
-            Tokens::Block(block_type, mut inside) => {
-                inside.push(character)
-            }
-        }
-    }
-} impl Display for Tokens {
-    fn fmt(&self, &mut formatter: Formatter) -> Result {
-        write!(formatter, "", self)
-    }
