cf4fe670ed74561979fff738805ce1d8ca55b312
Shakari Wade <saturn.kari.9@gmail.com>
2025-02-06 21:00:56 -0800

    Starting the creation of a simple uefi binary.

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use log::info;
+use uefi::prelude::*;
+
+#[entry]
+fn main() -> Status {
+    uefi::helpers::init().unwrap();
+    info!("Hello world!");
+    boot::stall(10_000_000);
+    
+    Status::SUCCESS
 }
