12427166fc7630d3f68dc43af3ccf75857f1f27b
Shakari Wade <saturn.kari.9@gmail.com>
2025-05-26 09:29:39 -0700

    Started working on bootloader part of the kernel.

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,23 +1,14 @@
-#![feature(uefi_std)]
+#![no_std]
+#![no_main]
 
-use std::os::uefi as uefi_std;
-use uefi::runtime::ResetType;
-use uefi::{Handle, Status};
-
-fn setup_uefi_crate() {
-    let st = uefi_std::env::system_table();
-    let ih = uefi_std::env::image_handle();
-    unsafe {
-        uefi::table::set_system_table(st.as_ptr().cast());
-
-        let ih = Handle::from_ptr(ih.as_ptr().cast()).unwrap();
-        uefi::boot::set_image_handle(ih);
-    }
+#[entry]
+fn main() -> Status {
+    efi_setup();
+    enter_runtime();
 }
 
-fn main() {
-    println!("Hello World from uefi_std");
-    setup_uefi_crate();
-    println!("UEFI-Version is {}", uefi::system::uefi_revision());
-    uefi::runtime::reset(ResetType::SHUTDOWN, Status::SUCCESS, None);
+fn enter_runtime() {
+}
+
+fn efi_setup() {
 }
