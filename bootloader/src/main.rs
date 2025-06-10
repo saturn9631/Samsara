1d4b01ef5acd716f17e78c97a3840a6918c17288
Shakari Wade <saturn.kari.9@gmail.com>
2025-06-10 01:06:13 -0700

    Coded alternate way of printing image while trying code a way to accesses the filesystem. Will continue to try as a means of loading the payload (kernel).

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -2,49 +2,72 @@
 #![no_std]
 
 use log::info;
-use uefi::boot::{self, SearchType, stall, open_protocol_exclusive, image_handle, locate_handle_buffer};
-use uefi::prelude::*;
-use uefi::proto::device_path::text::{
-    AllowShortcuts, DevicePathToText, DisplayOnly,
-};
+use uefi::{Status, entry, Identify, Result};
+use uefi::proto::BootPolicy;
+use uefi::proto::device_path::DevicePath;
+use uefi::proto::device_path::text::{AllowShortcuts, DevicePathToText, DisplayOnly};
 use uefi::proto::loaded_image::LoadedImage;
-use uefi::{Identify, Result};
-
+use uefi::proto::media::load_file::LoadFile;
+use uefi::proto::media::file::FileInfo;
+use uefi::boot::{SearchType, stall, open_protocol_exclusive, image_handle, locate_handle_buffer, get_image_file_system, exit_boot_services};
+//use uefi::data_types::PoolString;
+use uefi::proto::device_path::text::PoolString;
+ 
 #[entry]
 fn main() -> Status {
     uefi::helpers::init().unwrap();
 
-    print_image_path().unwrap();
+    //let file = getFile("");
+    let error_code = printImage();
+    info!("\"getFile\" returned with error code {}.", error_code);
 
-    stall(15_000_000);
+    info!("----------------Done----------------");
+    stall(30_000_000);
+
     Status::SUCCESS
 }
 
-fn print_image_path() -> Result {
-    let loaded_image =
-        open_protocol_exclusive::<LoadedImage>(image_handle())?;
+fn printImage() -> u8 {
+    let image = match open_protocol_exclusive::<LoadedImage>(image_handle()) {
+        Ok(some_image) => some_image,
+        Err(error) => {
+            info!("Could not get image due to {} error.", error);
+            return 1;
+        },
+    };
+    //let device_path_handle = locate_handle_buffer(SearchType::ByProtocol(&DevicePath::GUID));
+    let path_reference = match image.file_path() {
+        None => {
+            info!("Could not get image file path (it is possible it does not have one).");
+            return 2;
+        },
+        Some (Path) => Path,
+    };
+    let image_name = match path_reference.to_string(DisplayOnly(true), AllowShortcuts(true)) { 
+        Ok(name) => name,
+        Err(error) => {
+            info!("Could not open path due to {} error", error);
+            return 3;
+        },
+    };
+    info!("The current file path is: {}", image_name);
 
-    let device_path_to_text_handle = *locate_handle_buffer(
-        SearchType::ByProtocol(&DevicePathToText::GUID),
-    )? //DevicePath may not have a guid
-    .first()
-    .expect("DevicePathToText is missing");
-
-    let device_path_to_text = open_protocol_exclusive::<DevicePathToText>(
-        device_path_to_text_handle,
-    )?;
-
-    let image_device_path =
-        loaded_image.file_path().expect("File path is not set");
-    let image_device_path_text = device_path_to_text
-        .convert_device_path_to_text(
-            image_device_path,
-            DisplayOnly(true),
-            AllowShortcuts(false),
-        )
-        .expect("convert_device_path_to_text failed");
-
-    info!("Image path: {}", &*image_device_path_text);
-    Ok(())
+    0
 }
 
+fn getFile() {
+    let mut filesystem = match get_image_file_system(image_handle()) {
+        Ok(fs) => fs,
+        Err(error) => {
+            info!("Could not open path due to {} error", error);
+            return;
+        },
+    };
+    let root = match filesystem.open_volume() {
+        Ok(directory) => directory,
+        Err(error) => {
+            info!("Could not open path due to {} error", error);
+            return;
+        },
+    };
+}
