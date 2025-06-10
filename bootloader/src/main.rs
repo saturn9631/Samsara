717d6a80ec44d12c36e9b4c8c2280be7b2ef7232
Shakari Wade <saturn.kari.9@gmail.com>
2025-06-10 14:19:42 -0700

    Started working on file search.

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -8,7 +8,8 @@ use uefi::proto::device_path::DevicePath;
 use uefi::proto::device_path::text::{AllowShortcuts, DevicePathToText, DisplayOnly};
 use uefi::proto::loaded_image::LoadedImage;
 use uefi::proto::media::load_file::LoadFile;
-use uefi::proto::media::file::FileInfo;
+use uefi::proto::media::file::{File, Directory, RegularFile, FileInfo};
+use uefi::proto::media::fs::SimpleFileSystem;
 use uefi::boot::{SearchType, stall, open_protocol_exclusive, image_handle, locate_handle_buffer, get_image_file_system, exit_boot_services};
 //use uefi::data_types::PoolString;
 use uefi::proto::device_path::text::PoolString;
@@ -17,9 +18,9 @@ use uefi::proto::device_path::text::PoolString;
 fn main() -> Status {
     uefi::helpers::init().unwrap();
 
-    //let file = getFile("");
-    let error_code = printImage();
+    let error_code = print_image();
     info!("\"getFile\" returned with error code {}.", error_code);
+    let file = getFile("");
 
     info!("----------------Done----------------");
     stall(30_000_000);
@@ -27,7 +28,7 @@ fn main() -> Status {
     Status::SUCCESS
 }
 
-fn printImage() -> u8 {
+fn print_image() -> u8 {
     let image = match open_protocol_exclusive::<LoadedImage>(image_handle()) {
         Ok(some_image) => some_image,
         Err(error) => {
@@ -55,19 +56,30 @@ fn printImage() -> u8 {
     0
 }
 
-fn getFile() {
-    let mut filesystem = match get_image_file_system(image_handle()) {
+fn getFile(filename: &str) -> Option<()> {
+    let filesystem: Vec<Box<File>> = Vec::new();
+    let mut filesystem_access = match get_image_file_system(image_handle()) {
         Ok(fs) => fs,
         Err(error) => {
-            info!("Could not open path due to {} error", error);
-            return;
+            info!("Could not open path due to {} error. First match.", error);
+            return None;
         },
     };
-    let root = match filesystem.open_volume() {
+    let root = match filesystem_access.open_volume() {
         Ok(directory) => directory,
         Err(error) => {
-            info!("Could not open path due to {} error", error);
-            return;
+            info!("Could not open path due to {} error. Second match.", error);
+            return None;
         },
     };
+    info!("The root directory is: {}", &root);
+    let next = match root.read_entry_boxed() {
+        Ok(optional_box) => optional_box,
+        Err(error) => {
+            info!("Could not open path due to {} error. Third match.", error);
+            return None;
+        },
+    };
+
+    Some(())
 }
